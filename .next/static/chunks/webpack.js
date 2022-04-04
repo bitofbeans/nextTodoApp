cf33113675290b58b2110609f3c469057a87f10e
bitofbeans <benas4sun@gmail.com>
2022-04-03 21:25:00 -0500

    Readded configs and reset project

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d41d9369d4d8140a"; }
+/******/ 		__webpack_require__.h = function() { return "1bc41caf22b54fa4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,9 @@
-function Home({ data }) {
-
-    const name = data.name;
+function Home() {
     return (
-        <main>
-            <h1>Home</h1>
-            <p>His name was {name}</p>
-        </main>
+        <div>
+            <h1>Next JS Todo List</h1>
+        </div>
     );
-}
-
-export async function getServerSideProps() {
-    // Fetch data from external API
-    const res = await fetch(`http://localhost:3000/api`);
-    const data = await res.json();
-
-    // Pass data to the page via props
-    return { props: { data } };
 }
 
 export default Home;
